f00f8fcb255eb802caca534b28358fc07e7067b0
bertani05 <felipebertani05@gmail.com>
2023-04-15 16:40:00 -0300

    Atv.18

--- lição18.py
+++ lição18.py
@@ -1,1 +1,10 @@
-n1 = 2
+from datetime import date
+at = date.today().year
+n = int(input('Digite o ano que você nasceu: '))
+i = at - n
+
+if i >= 16:
+    print('Podera Votar')
+
+else:
+    print('Espere um pouco tente ano que vem')
