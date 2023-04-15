a97c465f6ea0cb644782d05d75f9579c2137c631
bertani05 <felipebertani05@gmail.com>
2023-04-15 17:09:48 -0300

    Atv

---
+++ atv.19.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor\n'))
+n2 = int(input('Digite outro valor\n'))
+print(50 * '=')
+if n1 > n2:
+    print(f'O número maior é {n1}')
+else:
+    print(f'O número maior é {n2}')
