b6fd4fa11c29df541d574af653e2d23bd93146f8
bertani05 <felipebertani05@gmail.com>
2023-04-20 10:53:36 -0300

    Atv37

--- Atv.37.py
+++ Atv.37.py
@@ -1,3 +1,29 @@
-n = int(input('Poe favor digite quantos Kg de frutas:'))
-n1 = int(input('Digite se e Morango ou Maçã:'))
-if
+n1 = input('Digite 1 para Morango e 2 para Maçã ?\n')
+print(50 * '=')
+
+n2 = float(input('Digite quantos Kg de frutas?\n'))
+print(50 * '=')
+
+if n1 == '1':
+    if n2 <= 5:
+        v = n2 * 2.5
+        v = round(v, 2)
+        print(f'O valor total a pagar R${v}')
+    else:
+        v = n2 * 2.2
+        v = round(v, 2)
+        print(f'O valor total a pagar R${v}')
+
+elif n1 == '2':
+    if n2 <= 5:
+        v = n2 * 1.8
+        v = round(v, 2)
+        print(f'O valor total a pagar R${v}')
+    else:
+        v = n2 * 1.5
+        v = round(v, 2)
+        print(f'O valor total a pagar R${v}')
+
+else:
+    print('Tente Novamente...')
+
